58a64d9db8dac7c3a61ff660a9e5a390d0a47718
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-09-08 15:44:17 +0900

    lab1

--- Lab1/src/academy/pocu/comp2500/lab1/Lab1.java
+++ Lab1/src/academy/pocu/comp2500/lab1/Lab1.java
@@ -4,7 +4,6 @@ public class Lab1 {
     public static int operations(int a, int b) {
         return a + b;
     }
-
     public static void main(String[] args){
         int a = operations(2,3);
         System.out.println("2 + 3 = " + a);
--- src/academy/pocu/comp2500/example2.java
+++ src/academy/pocu/comp2500/example2.java
@@ -2,12 +2,17 @@ package academy.pocu.comp2500;
 
 
 public class example2 {
-    public static int add(int a, int b){
+    public static int operations(int a, int b) {
         return a + b;
     }
-    public static void main(String[] aegs){
-        int a = add(3,2);
-        System.out.println(a);
 
+    public static void main(String[] args){
+        int a = operations(2,3);
+        System.out.println("2 + 3 = " + a);
+        int b = operations(-2, -3);
+        System.out.println("(-2) + (-3) = " + b);
+        int c = operations(-2, 3);
+        System.out.println("(-2) + 3 = " + c);
     }
+
 }
