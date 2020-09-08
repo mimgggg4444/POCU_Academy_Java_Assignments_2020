565d0d6f7cbd0e4f2e1a73c485b126282bdab65e
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-09-08 20:21:10 +0900

    lab1

--- Lab1/src/academy/pocu/comp2500/lab1/add.java
+++ Lab1/src/academy/pocu/comp2500/lab1/add.java
@@ -1,12 +1,12 @@
-package academy.pocu.comp2500.test;
+package academy.pocu.comp2500.lab1.app;
+
 
 public class add {
     public int num1;
     public int num2;
 
-    public add(int a, int b){
+    public add(int a, int b) {
         this.num1 = num1;
         this.num2 = num2;
     }
-
 }
---
+++ Lab1/src/academy/pocu/comp2500/lab1/main.java
@@ -0,0 +1,1 @@
+package academy.pocu.comp2500.lab1.app;
