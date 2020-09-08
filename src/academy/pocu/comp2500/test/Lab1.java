9366c2d2b4d3dcd61cd4335542366039fb3cfbcc
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-09-08 19:21:54 +0900

    lab1

--- src/academy/pocu/comp2500/test/Lab1.java
+++ src/academy/pocu/comp2500/test/Lab1.java
@@ -1,11 +1,17 @@
-package academy.pocu.comp2500.lab1.app;
+package academy.pocu.comp2500.test;
+
 
 class add {
     public static void main(String[] args) {
-        int a = add(2 + 3)
+        int a = add(2, 3);
+        System.out.println();
+
     }
 
     public static int add(int a, int b) {
         return a + b;
     }
+
+
 }
+
---
+++ src/academy/pocu/comp2500/test/test2.java
@@ -0,0 +1,16 @@
+package academy.pocu.comp2500.test;
+
+public class test2 {
+    public static void main(String[] args) {
+        int addTwoPositives = add(2, 3);
+        int addTwoNegatives = add(-2, -3);
+        int addPositiveAndNegative = add(2, -3);
+        System.out.println(String.format("AddTwoPositives(2 + 3 = %s)", addTwoPositives));
+        System.out.println(String.format("AddTwoPositives((-2) + (-3) = %s)", addTwoNegatives));
+        System.out.println(String.format("AddTwoPositives(2 + (-3) = %s)", addPositiveAndNegative));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
