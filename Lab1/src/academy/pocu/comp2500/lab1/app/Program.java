8eddc36351f5dbfbf8d1dc65967046b3a4799caa
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-09-08 14:04:38 +0900

    lab1

--- Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
+++
@@ -1,8 +0,0 @@
-package academy.pocu.comp2500.lab1.app;
-
-public class Program {
-
-    public static void main(String[] args) {
-        // Write your code here
-    }
-}
---
+++ src/academy/pocu/comp2500/Main.java
@@ -0,0 +1,8 @@
+package academy.pocu.comp2500;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("hi");
+    }
+}
