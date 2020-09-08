e37ed4089b32b4846176436669d0065186050f60
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-09-08 17:10:48 +0900

    lab1

--- src/academy/pocu/comp2500/Lab1.java
+++ src/academy/pocu/comp2500/Lab1.java
@@ -1,6 +1,14 @@
 package academy.pocu.comp2500;
 
 public class Lab1 {
+    public static void main(String[] args){
+        int addTwoPositives = add(2,3);
+        int addTwoNegatives = add(-2,-3);
+        int addPositiveAndNegative = add(2,-3);
+        System.out.println(String.format("AddTwoPositives(2 + 3 = %s)", addTwoPositives));
+        System.out.println(String.format("AddTwoPositives((-2) + (-3) = %s)", addTwoNegatives));
+        System.out.println(String.format("AddTwoPositives(2 + (-3) = %s)", addPositiveAndNegative));
+    }
     public static int add(int a, int b){
         return a + b;
     }
